4dc0e51bfaa98fc9a99e0e4f475cb0958f614c47
Arturs Pusko <ruback105@gmail.com>
2025-11-29 12:16:30 +0200

    init commit

---
+++ js/01.js
@@ -0,0 +1,33 @@
+// Первый тип данных - число (number)
+// 10
+// 1) Сосчитать 12 * 9 + 5 
+console.log(12*9+5)
+
+// // Второй тип данных - текст (string)
+// 'Hello'
+// "Anastasija"
+// `Mamedbekova`
+// console.log(typeof 'Hello')
+// console.log(typeof "Anastasija")
+// console.log(typeof `Mamedbekova`)
+// 2) Вывести в консоль "Hello Anastasija" "Hello" 'Anastasija'
+console.log(" Hello" + " Anastasija ")
+
+// // Третий тип данных - правда или ложь (boolean)
+// true 
+// false
+// console.log(typeof true)
+// console.log(typeof false)
+// 3) Вывести в консоль true,false
+console.log(true,false)
+console.log(true,false, "sosal", 300)
+console.log(typeof true, typeof false, typeof "sosal", typeof 300)
+console.log(300+"200")
+// // Четвертый тип данных - null
+// null
+// console.log(typeof null)
+
+// // Пятый тип данных - undefined
+// undefined
+// console.log(typeof undefined)
+
---
+++ js/02.js
@@ -0,0 +1,60 @@
+// arrays, objects, functions, variables
+
+
+// Массив (array)
+// ["Шиба ину", "Австралийская овчарка", "Немецкая овчарка"]
+// [0,30,60,90,120,150,180,210,240,270,300,330,360]
+// [true,false,null,undefined]
+// ["Шиба ину", 30, true, null, undefined]
+// [
+//     [1,2,3],
+//     [5,6,7],
+//     [1,7,3]
+// ]
+// console.log(Array.isArray(["Шиба ину", "Австралийская овчарка", "Немецкая овчарка"]))
+// console.log(["Шиба ину", "Австралийская овчарка", "Немецкая овчарка"])
+// console.log([
+//          [1,2,3],
+//          [5,6,7],
+//          [1,7,3]
+//      ][1][1])
+
+// Variables
+
+// const x = 5
+// const y = 10
+
+// let x = 5
+// let y = 10
+
+// var x = 5
+// var y = 10
+
+// console.log(x+y)
+
+// const name = "Anastasija"
+// const surname = "Mamedbekova"
+
+// console.log(name, surname)
+
+// const dogBreeds = ["Шиба ину", "Австралийская овчарка", "Немецкая овчарка"]
+
+// console.log(dogBreeds)
+
+// const matrix = [
+//         [1,2,3],
+//         [5,6,7],
+//         [1,7,3]
+//     ];
+
+// console.log(matrix[1][1])
+
+// 1.sozdatj massiv s opisanijem menja
+// 2.vitashitj fam i years
+// 3.slozitj years and weight
+// 4.viovvesti v cons log vsem privet, menja zovut anastasija mne 26
+
+const proMenja=["Anastasija", "Mamedova", 26, 55,"rabotaju"]
+console.log(proMenja[1],proMenja[2])
+console.log(proMenja[2]+proMenja[3])
+console.log (`Vsem privet, menja zovut ${proMenja[0]} mne ${proMenja[2]} let`)
